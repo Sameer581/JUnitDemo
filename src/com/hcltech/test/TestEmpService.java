83f797ee30d52f7959c9a59200c21d4c3d752066
Sameer <sameersid9897@gmail.com>
2026-04-02 15:21:03 +0530

    corrected class

--- src/com/hcltech/test/TestEmpService.java
+++ src/com/hcltech/test/TestEmpService.java
@@ -1,6 +1,7 @@
 package com.hcltech.test;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
 
 import com.hcltech.service.EmpService;
 import com.hcltech.service.EmpService.Emp;
@@ -10,46 +11,45 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.jupiter.api.BeforeAll;
 public class TestEmpService {
 
-	public static EmpService service = EmpService.getServiceInstance();
-	public static Emp emp1;
-	public static List<String> expectedLst;
-	
-	@BeforeAll
-	public static void beforeAll() {
-		emp1 = EmpService.map.get(1001);
-		expectedLst = Arrays.asList("zaid", "sam", "david");
-	}
-	
-	
-	@Test
-	public void testFindById1() {
-		assertEquals(emp1, service.findByEmpId(1001));
-	}
-	
-	@Test
-	public void testFindById2() {
-		assertNotNull(service.findByEmpId(1001));
-	}
-	
-	@Test
-	public void testFindById3() {
-		assertNull(service.findByEmpId(2001));
-	}
-	
-	@Test
-	public void testServiceSingleton() {
-		assertSame(service, EmpService.getServiceInstance());
-	}
-	
-	@Test
-	public void testEmpNames1() {
-		assertTrue(service.getEmpNames().size() == expectedLst.size());
-	
-	@Test
-	public void testEmpNames2() {
-		assertArrayEquals(expectedLst.toArray(), service.getEmpNames().toArray());
-	}	
+    public static EmpService service = EmpService.getServiceInstance();
+    public static Emp emp1;
+    public static List<String> expectedLst;
+
+    @BeforeAll
+    public static void beforeAll() {
+        emp1 = EmpService.map.get(1001);
+        expectedLst = Arrays.asList("manoj", "sam", "david");
+    }
+
+    @Test
+    public void testFindById1() {
+        assertEquals(emp1, service.findByEmpId(1001));
+    }
+
+    @Test
+    public void testFindById2() {
+        assertNotNull(service.findByEmpId(1001));
+    }
+
+    @Test
+    public void testFindById3() {
+        assertNull(service.findByEmpId(2001));
+    }
+
+    @Test
+    public void testServiceSingleton() {
+        assertSame(service, EmpService.getServiceInstance());
+    }
+
+    @Test
+    public void testEmpNames1() {
+        assertTrue(service.getEmpNames().size() == expectedLst.size());
+    }
+
+    @Test
+    public void testEmpNames2() {
+        assertArrayEquals(expectedLst.toArray(), service.getEmpNames().toArray());
+    }
 }
